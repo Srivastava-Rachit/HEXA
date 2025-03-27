9bc9133f68efeef2e49524b7817adfddaa0f0fdc
Srivastava-Rachit <srivastavarachit141@gmail.com>
2025-03-28 02:13:18 +0530

    Update adminRoute.js

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,7 @@
 import express from 'express'
 import { loginAdmin } from '../controllers/adminController.js'
 
-
 const adminRouter = express.Router()
-
-// adminRouter.post("/registera", registerAdmin)
 adminRouter.post("/logina", loginAdmin)
 
 export default adminRouter;
